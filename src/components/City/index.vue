b4851b28dff114301847889326cb238cb65da0ee
e <894399743@qq.com>
2019-12-11 20:05:47 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="city_body">
-		<div class="city_list">
+		<!-- <div class="city_list">
 			<div class="city_hot">
 				<h2>热门城市</h2>
 				<ul class="clearfix">
@@ -79,13 +79,105 @@
 				<li>D</li>
 				<li>E</li>
 			</ul>
+		</div> -->
+		<div class="city_list">
+			<div class="city_hot">
+				<h2>热门城市</h2>
+				<ul class="clearfix">
+					<li v-for="item in hotList" :key="item.id">{{ item.nm }}</li>
+				</ul>
+			</div>
+			<div class="city_sort" ref="city_sort">
+				<div v-for="item in cityList" :key="item.index" >
+					<h2>{{item.index}}</h2>
+					<ul>
+						<li v-for="itemList in item.list" :key="itemList.id">{{itemList.nm}}</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="city_index">
+			<ul>
+				<li v-for="(item,index) in cityList" :key="item.index"  @touchstart = "handlerToIndex(index)">{{item.index}}</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'City'
+	name: 'City',
+	data() {
+		return {
+			cityList: [],
+			hotList: []
+		};
+	},
+	mounted() {
+		this.axios.get('/api/cityList').then(res => {
+			//console.log(res);
+			var msg = res.data.msg;
+			if (msg === 'ok') {
+				var cities = res.data.data.cities;
+				//[{index:'A',list:[{nm:'阿查',id:123}]}]
+				var { cityList, hotList } = this.formatCityList(cities);
+				this.cityList = cityList;
+				this.hotList = hotList;
+			}
+		});
+	},
+	methods: {
+		formatCityList(cities) {
+			var cityList = [];
+			var hotList = [];
+			for (var i = 0; i < cities.length; i++) {
+				if (cities[i].isHot === 1) {
+					hotList.push(cities[i]);
+				}
+			}
+			for (var i = 0; i < cities.length; i++) {
+				var firstLetter = cities[i].py.substring(0, 1).toUpperCase();
+				if (toCom(firstLetter)) {
+					//新添加索引
+					cityList.push({ index: firstLetter, list: [{ nm: cities[i].nm, id: cities[i].id }] });
+				} else {
+					//累加到已有索引中
+					for (var j = 0; j < cityList.length; j++) {
+						if (cityList[j].index === firstLetter) {
+							cityList[j].list.push({ nm: cities[i].nm, id: cities[i].id });
+						}
+					}
+				}
+			}
+			cityList.sort((n1, n2) => {
+				if (n1.index > n2.index) {
+					return 1;
+				} else if (n1.index < n2.index) {
+					return -1;
+				} else {
+					return 0;
+				}
+			});
+			function toCom(firstLetter) {
+				for (var i = 0; i < cityList.length; i++) {
+					if (cityList[i].index === firstLetter) {
+						return false;
+					}
+				}
+				return true;
+			}
+			// console.log(cityList);
+			// console.log(hotList);
+			return {
+				cityList,
+				hotList
+			};
+		},
+		handlerToIndex(index){
+			var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+			this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+		}
+	}
 };
 </script>
 
@@ -157,6 +249,6 @@ export default {
 	text-align: center;
 	position: fixed;
 	right: 0;
-	top: 50%;
+	top: 18%;
 }
 </style>
